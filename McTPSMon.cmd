63be5a8e4bf9497980683b04dfd9eb50dacfcf24
Jefta Dirksen <JeftaDirksen@gmail.com>
2021-01-08 10:14:58 +0100

    Config / Cleanup / Comments

--- McTPSMon.cmd
+++ McTPSMon.cmd
@@ -1,12 +1,30 @@
 @echo off
 setlocal
-mcrcon -p password "list" > output.tmp
-for /f "tokens=3" %%i in (output.tmp) do set count=%%i
-if %count% equ 0 exit /b
-for /f "tokens=2 delims=:" %%i in (output.tmp) do set players=%%i
-mcrcon -p password "debug start" > output.tmp
-timeout /t 5 >nul
-mcrcon -p password "debug stop" > output.tmp
-for /f "tokens=2 delims=(," %%i in (output.tmp) do set tps=%%i
-echo %date:~3,12% %time:~0,5% %tps% (%count%: %players:~1%)>> McTPSMon.log
-del output.tmp
+
+:: Config
+set rconPassword=password
+set debugPath=D:\CleanMcJava\debug
+set debugCleanupDays=14
+set tpsMeasureSeconds=5
+set logFile=McTPSMon.log
+set tempFile=output.tmp
+
+:: Get players
+mcrcon -p %rconPassword% "list" > %tempFile%
+for /f "tokens=3" %%i in (%tempFile%) do set count=%%i
+if %count% equ 0 goto end
+for /f "tokens=2 delims=:" %%i in (%tempFile%) do set players=%%i
+
+:: Get TPS
+mcrcon -p %rconPassword% "debug start" > %tempFile%
+timeout /t %tpsMeasureSeconds% >nul
+mcrcon -p %rconPassword% "debug stop" > %tempFile%
+for /f "tokens=2 delims=(," %%i in (%tempFile%) do set tps=%%i
+
+:: Output to log
+echo %date:~3,12% %time:~0,5% %tps% (%count%: %players:~1%)>> %logFile%
+
+:: Cleanup
+:end
+forfiles /p %debugPath% /m profile-results-*.txt /d -%cleanupAgeDays% /c "cmd /c del @file" >nul 2>&1
+del %tempFile%
