ae8f6b227f72f588f952aa2e85a3d665e3b3c171
Jefta Dirksen <JeftaDirksen@gmail.com>
2021-01-10 20:51:36 +0100

    Output

--- McTPSMon.cmd
+++ McTPSMon.cmd
@@ -7,7 +7,7 @@ for /f %%a in (McTPSMon-config.ini) do set %%a
 :: Get players
 mcrcon -p %rconPassword% "list" > %tempFile%
 for /f "tokens=3" %%i in (%tempFile%) do set count=%%i
-if %count% equ 0 goto end
+if %count% equ 0 goto server_empty
 for /f "tokens=2 delims=:" %%i in (%tempFile%) do set players=%%i
 
 :: Get TPS
@@ -16,9 +16,8 @@ timeout /t %tpsMeasureSeconds% >nul
 mcrcon -p %rconPassword% "debug stop" > %tempFile%
 for /f "tokens=2 delims=(," %%i in (%tempFile%) do set tps=%%i
 
-:: Output to console/log
-echo %date:~3,12% %time:~0,5% %tps% (%count%: %players:~1%)
-echo %date:~3,12% %time:~0,5% %tps% (%count%: %players:~1%)>> %logFile%
+:: Output
+call :output "%tps% (%count%: %players:~1%)"
 
 :: Notify admin (in game)
 if %tps% lss %notifyTpsBelow% ^
@@ -33,8 +32,17 @@ if %tps% lss %notifyTpsBelow% ^
   --form-string "message=TPS: %tps%" ^
   https://api.pushover.net/1/messages.json >nul
 
-:: Cleanup
-:end
+:server_empty
+echo Server empty
+goto cleanup
+
+:cleanup
 forfiles /p %debugPath% /m profile-results-*.txt /d -%cleanupAgeDays% ^
   /c "cmd /c del @file" >nul 2>&1
 del %tempFile%
+goto :eof
+
+:output
+echo %1
+echo %date:~3,12% %time:~0,5% %1>> %logFile%
+goto :eof
