44c304bf2868ebaddeaaa746d9736fca6d8860c2
Jefta Dirksen <JeftaDirksen@gmail.com>
2021-01-10 20:09:30 +0100

    Formatting

--- McTPSMon.cmd
+++ McTPSMon.cmd
@@ -29,12 +29,20 @@ for /f "tokens=2 delims=(," %%i in (%tempFile%) do set tps=%%i
 echo %date:~3,12% %time:~0,5% %tps% (%count%: %players:~1%)>> %logFile%
 
 :: Notify admin (in game)
-if %tps% lss %notifyTpsBelow% mcrcon -p %rconPassword% -s "tellraw %notifyAdmin% {\"text\":\"TPS %tps%\",\"color\":\"red\"}"
+if %tps% lss %notifyTpsBelow% ^
+  mcrcon -p %rconPassword% -s ^
+  "tellraw %notifyAdmin% {\"text\":\"TPS %tps%\",\"color\":\"red\"}"
 
 :: Pushover notification
-if %tps% lss %notifyTpsBelow% curl -s --form-string "token=%pushoverAppToken%" --form-string "user=%pushoverUserKey%" --form-string "message=TPS: %tps%" https://api.pushover.net/1/messages.json >nul
+if %tps% lss %notifyTpsBelow% ^
+  curl -s ^
+  --form-string "token=%pushoverAppToken%" ^
+  --form-string "user=%pushoverUserKey%" ^
+  --form-string "message=TPS: %tps%" ^
+  https://api.pushover.net/1/messages.json >nul
 
 :: Cleanup
 :end
-forfiles /p %debugPath% /m profile-results-*.txt /d -%cleanupAgeDays% /c "cmd /c del @file" >nul 2>&1
+forfiles /p %debugPath% /m profile-results-*.txt /d -%cleanupAgeDays% ^
+  /c "cmd /c del @file" >nul 2>&1
 del %tempFile%
