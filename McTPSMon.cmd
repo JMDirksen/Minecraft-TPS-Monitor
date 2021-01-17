d87dd32182116fc3d83a292914aeba716fe35ed6
Jefta Dirksen <JeftaDirksen@gmail.com>
2021-01-17 18:10:02 +0100

    Fix: count not showing

--- McTPSMon.cmd
+++ McTPSMon.cmd
@@ -16,7 +16,7 @@ mcrcon.exe -s list
 
 :: Get player count
 for /f "tokens=3" %%i in ('mcrcon.exe list') do set count=%%i
-echo %yy%,%mm%,%dd%,%h%,%m%,%count%>> %UserCountDB%
+echo %yy%,%mm%,%dd%,%h%,%m%,%count% >> %UserCountDB%
 if %count% equ 0 goto server_empty
 
 :: Get players
